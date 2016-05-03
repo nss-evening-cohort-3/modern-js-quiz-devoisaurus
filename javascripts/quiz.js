93421447a3222e8ceff9b8024187f407458e6f6d
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-05-02 20:20:24 -0500

    set up browserify

---
+++ javascripts/quiz.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var $ = require("jquery"),
+		battle = require("./battle.js"),
+		robots = require("./robots,js"),
+		modifications = require("./modifications,js"),
+		weapons = require("./weapons.js");
